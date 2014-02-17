dc101c110c54d3e63f8396773affe017d3c55a1e
Lamb <lambgao@me.com>
2014-02-17 22:49:36 +0800

    MappingTemplate

---
+++ src/main/java/org/mynah/oblatum/App.java
@@ -0,0 +1,13 @@
+package org.mynah.oblatum;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
