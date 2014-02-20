636f89a978aa352fd1b1bfa2ead1aad68d424b56
Lamb <lambgao@me.com>
2014-02-20 17:51:45 +0800

    常量移动到同一个文件里面

--- src/main/java/org/mynah/oblatum/jdbc/MappingJdbcTemplate.java
+++ src/main/java/org/mynah/oblatum/jdbc/MappingJdbcTemplate.java
@@ -13,6 +13,7 @@ import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
+import static org.mynah.oblatum.util.Constants.*;
 
 public class MappingJdbcTemplate implements MappingJdbcOperations {
 
@@ -20,25 +21,6 @@ public class MappingJdbcTemplate implements MappingJdbcOperations {
      * Logger available to subclasses
      */
     protected final Log logger = LogFactory.getLog(getClass());
-    public static final String CLASS = "class";
-    public static final String SPACE = " ";
-    public static final String LEFT_BRACKET = "(";
-    public static final String RIGHT_BRACKET = ")";
-    public static final String COLON = ":";
-    public static final String COMMA = ",";
-    public static final String INSERT = "insert";
-    public static final String VALUES = "values";
-    public static final String UNDERLINE = "_";
-    public static final String SELECT = "select";
-    public static final String ASTERISK = "*";
-    public static final String FROM = "from";
-    public static final String WHERE = "where";
-    public static final String IDENTICAL = "1=1";
-    public static final String EQUAL = "=";
-    public static final String AND = "and";
-    public static final String DELETE = "delete";
-    public static final String UPDATE = "update";
-    public static final String SET = "set";
 
     private NamedParameterJdbcTemplate namedJdbcTemplate;
 
